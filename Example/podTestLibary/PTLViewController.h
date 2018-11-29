ed7687cd2c838566307f4807513dafebf6478fae
zouhuanlin <zouhuanlin@51xianqu.net>
2018-11-29 16:08:38 +0800

    Initial commit

---
+++ Example/podTestLibary/PTLViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTLViewController.h
+//  podTestLibary
+//
+//  Created by zouhuanlin on 11/29/2018.
+//  Copyright (c) 2018 zouhuanlin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PTLViewController : UIViewController
+
+@end
---
+++ Example/podTestLibary/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  podTestLibary
+//
+//  Created by zouhuanlin on 11/29/2018.
+//  Copyright (c) 2018 zouhuanlin. All rights reserved.
+//
+
+@import UIKit;
+#import "PTLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PTLAppDelegate class]));
+    }
+}
